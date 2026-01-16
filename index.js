c1621a42e8f11a441a290b0ad457c7dd241811b8
donweslay-voidex <donwet10@gmail.com>
2026-01-15 17:41:16 -0800

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
-    const mc = net.connect(25565, '147.135.104.179');
+    const mc = net.connect(15014, '147.135.104.179');
     
     ws.on('message', (data) => mc.write(data));
     mc.on('data', (data) => ws.send(data));
